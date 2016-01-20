974c3820c4518f343702806aa8c49a5db0f470da
Artyom Desyatnikov <l0nikov@ya.ru>
2016-01-20 14:44:27 +0500

    prints voices

--- app/Main.hs
+++ app/Main.hs
@@ -2,12 +2,19 @@ module Main where
 
 import Lib
 import Sound.MIDI.File
-import qualified Data.EventList.Relative.TimeBody as EventList
+import qualified Data.EventList.Relative.TimeBody as RelEvList
+import qualified Data.EventList.Absolute.TimeBody as AbsEvList
+import Sound.MIDI.File.Event
+import Data.Foldable
 
-printTrackInfo pairs = do
-  print $ map (\(t, e) -> e) pairs
+flatMap :: Data.Foldable.Foldable t => (a -> t b) -> [a] -> [b]
+flatMap f = concatMap (Data.Foldable.toList . f)
 
 main :: IO ()
 main = do 
   midi <- openSammy
-  mapM_ (printTrackInfo . EventList.toPairList) (getTracks midi)
+  mapM_ (print
+                    . flatMap (\(t, ev) -> maybeVoice ev)
+                    . AbsEvList.toPairList
+                    . RelEvList.toAbsoluteEventList 0
+                    ) (getTracks midi)
