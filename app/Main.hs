d8fa26f8c60bf3a59dfc71f63ee68d5c2728ca40
Artyom Desyatnikov <l0nikov@ya.ru>
2016-01-21 04:27:34 +0500

    toPresses compiles

--- app/Main.hs
+++ app/Main.hs
@@ -2,19 +2,16 @@ module Main where
 
 import Lib
 import Sound.MIDI.File
+import Sound.MIDI.File.Load
 import qualified Data.EventList.Relative.TimeBody as RelEvList
-import qualified Data.EventList.Absolute.TimeBody as AbsEvList
-import Sound.MIDI.File.Event
-import Data.Foldable
+import Data.EventList.Absolute.TimeBody as AbsEvList
+import Sound.MIDI.Message.Channel
+import qualified Sound.MIDI.Message.Channel.Voice as Voice
+import Sound.MIDI.File.Event as Event
+import Data.Foldable as Foldable
 
-flatMap :: Data.Foldable.Foldable t => (a -> t b) -> [a] -> [b]
-flatMap f = concatMap (Data.Foldable.toList . f)
 
 main :: IO ()
-main = do 
-  midi <- openSammy
-  mapM_ (print
-                    . flatMap (\(t, ev) -> maybeVoice ev)
-                    . AbsEvList.toPairList
-                    . RelEvList.toAbsoluteEventList 0
-                    ) (getTracks midi)
+main = do
+  midi <- fromFile "samples/sammy.mid"
+  Foldable.mapM_ (print . getVoiceEvents) (getTracks midi)
